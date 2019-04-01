f5c9ce8cfee4c4eeda7a465775c748aaf7e527a5
Ernie Costa <erniecosta@gmail.com>
2019-03-31 21:10:29 -0400

    Revert "Revert "Update Tesla.psm1""
    This reverts commit 724a46cf32bc7d3c401b9e7656a1f335f6f4441d.

--- Tesla.psm1
+++ Tesla.psm1
@@ -315,6 +315,8 @@ Set-Tesla
                      'climate_state',
                      'drive_state',
                      'gui_settings',
+                     'mobile_enabled',
+                     'nearby_charging_sites',
                      'vehicle_state',
                      'vehicle_config',
                      'vehicles'
@@ -364,15 +366,18 @@ Get-Tesla
         [parameter(Mandatory=$true,Position=0)]
         [ValidateSet('auto_conditioning_start',
                      'auto_conditioning_stop',
-                     'door_lock',
-                     'door_unlock',
-                     'charge_port_door_open',
                      'charge_max_range',
+                     'charge_port_door_open',
+                     'charge_port_door_close',
                      'charge_standard',
                      'charge_start',
                      'charge_stop',
+                     'door_lock',
+                     'door_unlock',
                      'flash_lights',
                      'honk_horn',
+                     'reset_valet_pin',
+                     'upcoming_calendar_entries',
                      'wake_up'
                      )]
         [string]$Command
